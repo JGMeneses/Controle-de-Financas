416b651c161d77affd37e4aa026a397a6a50fea4
JGMeneses <joaovictorcont15@gmail.com>
2023-03-17 08:26:57 -0300

    1.0.1

--- src/components/Grid/index.js
+++ src/components/Grid/index.js
@@ -3,7 +3,11 @@ import GridItem from "../GridItem"
 import * as C from "./style"
 
 const Grid = ({itens, setItens}) => {
-
+    const onDelete = (ID) =>{
+        const newArray = itens.filter((transaction) => transaction.id !== ID);
+        setItens(newArray);
+        localStorage.setItem("transactions", JSON.stringify(newArray));
+    };
     return (
        <C.Table>
         <C.Thead>
@@ -18,7 +22,7 @@ const Grid = ({itens, setItens}) => {
         </C.Thead>
         <C.Tbody>
             {itens ?.map((item, index) =>
-                <GridItem key={index} item={item}/>
+                <GridItem key={index} item={item} onDelete={onDelete}/>
             )}
         </C.Tbody>
        </C.Table>
